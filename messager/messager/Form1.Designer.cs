043b79e8a7eddf647e233f40c15c26b8b36c0a11
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2022-06-27 23:19:17 +0430

    Add files via upload

---
+++ messager/messager/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace messager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Vazir", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(304, 230);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 249);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(251, 25);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(272, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ارسال";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 284);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Vazir", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ messager/messager/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace messager
+{
+    public partial class Form1 : Form
+    {
+
+        //System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        private IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8080);
+        private int userId;
+        private static Socket server = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartClient();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendMessage(server, textBox2.Text);
+            textBox2.Text = "";
+        }
+
+        private void setText(string msg)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(setText), new object[] { msg });
+                return;
+            }
+            textBox1.AppendText("\r\n"+checkId(msg));
+        }
+
+        private string checkId(string msg)
+        {
+            string[] msgs = msg.Split(';');
+            if (Convert.ToInt32(msgs[0]) == userId)
+            {
+                return "شما : "+msgs[1];
+            }
+            return "کاربر " + Convert.ToInt32(msgs[0]) + " : " + msgs[1];
+        }
+
+        private void StartClient()
+        {
+           
+            try
+            {
+                //IPHostEntry host = Dns.GetHostEntry("localhsot");
+
+
+                try
+                {
+                    server.Connect(remoteEP);
+
+                    textBox1.Text = "به سرور" + server.RemoteEndPoint.ToString()+ " متصل شدید.";
+                    try
+                    {
+                        byte[] bytes = new byte[1024];
+
+                        int bytesRec = server.Receive(bytes);
+                        userId = Convert.ToInt32(Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                        this.Text = "کاربر : "+userId;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error");
+                    }
+
+                    Thread getMessage = new Thread(GetMessage);
+                    getMessage.Start();
+                }
+                catch(ArgumentNullException ane)
+                {
+                    MessageBox.Show("ArgumentNullException : "+ane.ToString());
+                }
+                catch(SocketException se)
+                {
+                    MessageBox.Show("SocketException : " + se.ToString());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Exception : " + e.ToString());
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void sendMessage(Socket sender, string msg)
+        {
+            byte[] bytes = new byte[1024];
+            byte[] message = Encoding.UTF8.GetBytes(msg);
+
+            int bytesSent = sender.Send(message);
+
+            //int bytesRec = sender.Receive(bytes);
+
+            //textBox1.Text += "\r\n " + Encoding.UTF8.GetString(bytes, 0, bytesRec);
+        }
+
+        private void textBox2_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+
+        private void GetMessage()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+
+                int bytesRec = server.Receive(bytes);
+
+                setText("\r\n" + Encoding.UTF8.GetString(bytes, 0, bytesRec));
+            }
+        }
+    }
+}
